49f19dd6b85806bbb8cac10e727b23fb9d3d6a1e
Garvit Kulshrestha <garvitindian@gmail.com>
2025-08-05 20:05:14 +0530

    Add main.go to root for go install

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/Garvitkul/s3mpc/internal/app"
+)
+
+// Version information (set by build flags)
+var (
+	Version   = "1.0.0"
+	BuildTime = "unknown"
+	GitCommit = "unknown"
+	GitBranch = "unknown"
+)
+
+func main() {
+	ctx := context.Background()
+	
+	app := app.NewApp()
+	if err := app.Run(ctx, os.Args[1:]); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
